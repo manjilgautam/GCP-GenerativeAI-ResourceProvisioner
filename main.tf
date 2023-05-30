bce946e496a3bdd1293e0058c42f00c18dc16eb3
manjilgautam <mgtm@google.com>
2023-05-30 10:39:36 -0500

    resource provisioner module

---
+++ main.tf
@@ -0,0 +1,15 @@
+module "existing_project" {
+  source = "./modules/data_science"
+
+  billing_account_id = "<BILLING_ID>"
+  organization_id    = "<ORGANIZATION_ID>"
+  folder_id          = "<FOLDER_ID>"
+
+  create_project       = false
+  project_id_prefix    = "<PROJECT_ID>"
+  enable_services      = false
+  
+  set_external_ip_policy          = false
+  set_shielded_vm_policy          = false
+  set_trustedimage_project_policy = false
+}
